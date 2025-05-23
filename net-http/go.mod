a13867cfcc747d9df6f61a63eea667d1914d8c6c
KhaQT <khaquachtrong74@gmail.com>
2025-05-24 03:26:31 +0700

    Create package net-http

---
+++ net-http/go.mod
@@ -0,0 +1,5 @@
+module simple-api/net_http
+
+go 1.24.3
+
+require github.com/dreamsofcode-io/nethttp v0.0.0-20240320161152-63ead0d466e6
---
+++ net-http/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+	"github.com/dreamsofcode-io/nethttp/monster"
+)
+
+func main(){
+	handler := &monster.Handler{}
+	router := http.NewServeMux()
+	router.HandleFunc("/item/{id}", func(w http.ResponseWriter, r *http.Request){
+		// something done here
+		// path parameters
+		id := r.PathValue("id")
+		w.Write([] byte("Received request for the item: " + id + "\n"))
+	})
+	// method based routing
+	router.HandleFunc("GET /monster", handler.Create)
+	router.HandleFunc("PUT /monster", handler.UpdateByID)
+	s := &http.Server{
+		Addr: ":8080",
+		Handler: router,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout: 10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	fmt.Println("Starting server on port: " + s.Addr)
+	log.Fatal(s.ListenAndServe())
+}
