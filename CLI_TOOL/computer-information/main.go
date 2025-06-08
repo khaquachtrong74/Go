ca9d2e0b76fe9a71e2a69c54d720072c8b164b3c
khat <khaquachtrong74@gmail.com>
2025-06-08 23:57:23 +0700

    Use package to get computer infor

---
+++ CLI_TOOL/computer-information/main.go
@@ -0,0 +1,39 @@
+package main
+import (
+		"fmt"
+
+    "github.com/shirou/gopsutil/cpu"
+    "github.com/shirou/gopsutil/disk"
+    "github.com/shirou/gopsutil/host"
+    "github.com/shirou/gopsutil/mem"
+
+)
+type SysInfo struct {
+    Hostname string `bson:hostname`
+    Platform string `bson:platform`
+    CPU      string `bson:cpu`
+    RAM      uint64 `bson:ram`
+    Disk     uint64 `bson:disk`
+}
+func main() {
+    hostStat, _ := host.Info()
+    cpuStat, _ := cpu.Info()
+    vmStat, _ := mem.VirtualMemory()
+    diskStat, _ := disk.Usage("/home/khat/Documents/")
+
+    info := new(SysInfo)
+
+    info.Hostname = hostStat.Hostname
+    info.Platform = hostStat.Platform
+    info.CPU = cpuStat[0].ModelName
+    info.RAM = vmStat.Total / 1024 / 1024
+    info.Disk = diskStat.Total / 1024 / 1024
+    fmt.Printf("%+v\n", info)
+		fmt.Println("Platform: ",hostStat.Platform, hostStat.PlatformVersion)
+		fmt.Println("CPU: ", cpuStat[0].ModelName)
+		fmt.Println(mem.SwapDevices())
+		fmt.Println(diskStat)
+
+}
+
+
